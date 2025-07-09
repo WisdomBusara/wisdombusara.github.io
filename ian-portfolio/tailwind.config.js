9745ad66205407d50a83aa96ca4bf8d3d860eb0e
Ian Kimani <ianmwaura@gmail.com>
2025-07-09 15:30:02 +0300

    ZONE 002

--- ian-portfolio/tailwind.config.js
+++ ian-portfolio/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // ➡️ Enable class-based dark mode toggle
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
